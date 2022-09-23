ddc55dc0d20d9b0a2f8c8ea72dd3f8847e37c3af
Kevin Li <kevin.li20021106@gmail.com>
2022-09-22 21:35:16 -0400

    Complete chapter 4 examples

--- hello.py
+++ hello.py
@@ -1,18 +1,32 @@
-from datetime import datetime
-
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+from forms import NameForm
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "dfjowiefhodsanfd"
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+        
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+
     return render_template(
-        "index.html", name="KevinLiTian", current_time=datetime.utcnow()
+        "index.html",
+        name=session.get("name"),
+        form=form,
     )
 
 
